6d8e56f962c524fb1c1dc776090a367311543979
Jorens Shtekels <jorens.stekels@gmail.com>
2024-03-10 19:19:00 +0200

    feat[prep]: role, player constructors

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,10 +1,13 @@
-#include "lib.hh"
-
 #include "prep/prep.hh"
+
+#include <vector>
 
 void run() {
     Room room1(1, "Room 1", 1710087364, RoomStatus::IN_PROGRESS);
     Room room2(2, "Room 2", 1710087384, RoomStatus::ENDED);
+    Role role1({Action::KILL, Action::HEAL});
+    std::vector<Action> actions = {Action::HEAL};
+    Role role2(actions);
 }
 
 int functionToTest(int a) {
